015f50ca327579e74210ba5260120959af0d2b71
BoweS2027 <bowe.siegelson2027@gbwl.org>
2026-01-29 19:26:12 +0000

    wsd

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+     // 1D array is a block of memory that stores a collection of data item called elements under one variable name
+     //how to declare an array and create
+     String[] apCompSci = new String[13];
+     double[] prices = new double[5]; // default values 0.0
+     String[] names = new String[5]; // null
+     int[] luckyNums = new int[10]; // 0
+     boolean[] yn = new boolean[3]; // false
    }
 }
