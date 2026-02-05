c2d011b351354fafbeedf944f1ab8f521e6a3061
BoweS2027 <bowe.siegelson2027@gbwl.org>
2026-02-05 19:08:03 +0000

    weihbud

--- Main.java
+++ Main.java
@@ -8,5 +8,22 @@ public class Main {
      String[] names = new String[5]; // null
      int[] luckyNums = new int[10]; // 0
      boolean[] yn = new boolean[3]; // false
+     double[] scores = {45.0, 20.0, 89.5, 97.8};
+     //look at one item using an index
+     System.out.println(scores[1]);
+     for(int i = 0; i < 4; i++){
+      System.out.println(scores[i]);
+     }
+     for(int i = 0; i < 4; i++){
+      scores[i]= scores[i]+10;
+      System.out.println("Score after cruve: " + scores[i]);
+     }
+     double sum = 0;
+     for(double score : scores){
+      sum+=score;
+     }
+     System.out.println(sum);
+     double avg = sum / scores.length;
+     System.out.println(avg);
    }
 }
